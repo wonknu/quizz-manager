37148a91bb5bc0b64c225702656d299c89b577ba
fabrice labbe <fabrice.labbe@adfab.fr>
2013-05-13 16:21:28 +0200

    Fix answer view page

--- app/scripts/utils/templating.js
+++ app/scripts/utils/templating.js
@@ -9,3 +9,18 @@ Handlebars.registerHelper('hasHTML', function (text, url)
 { 
 	return new Handlebars.SafeString(text);
 });
+
+/**
+ * Retrieve true answer from all the aswer possible in the array of answer for a question 
+ */
+Handlebars.registerHelper('trueAnswer', function (answers)
+{
+    var trueAnswer = '', i;
+    for (i = 0; i < answers.length; i++) {
+        if(answers[i].secret == "true"){
+            trueAnswer = answers[i].text;
+            break;
+        }
+    };
+    return trueAnswer;
+});
